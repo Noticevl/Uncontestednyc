56796cc915bf3805a687a4d36d655a7c2ce977ca
Noticevl <bluegrace17@gmail.com>
2025-09-02 16:07:42 -0400

    Add files via upload

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,10 +1,1 @@
-
-document.addEventListener('click', (e)=>{
-  if(e.target.matches('.faq-item button')){
-    const item = e.target.closest('.faq-item');
-    const ans = item.querySelector('.a');
-    const open = ans.style.display==='block';
-    document.querySelectorAll('.faq-item .a').forEach(a=>a.style.display='none');
-    ans.style.display = open ? 'none' : 'block';
-  }
-});
+document.addEventListener('click',e=>{const h=e.target.closest('.faqHead');if(h){h.parentElement.classList.toggle('open');}})
